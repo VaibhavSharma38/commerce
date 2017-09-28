00b87f364d68fbc570ee702d83e20876cc9eda97
vaibhavsharma38 <vaibhavsharma38@gmail.com>
2017-09-28 09:04:32 +0000

    changes made

--- lib/Tool/ItemList.php
+++ lib/Tool/ItemList.php
@@ -221,8 +221,8 @@ class Tool_ItemList extends \xepan\cms\View_Tool{
 
 
 		//load record according to sequence of order 
-		$item->setOrder('display_sequence','asc');
-		// $item->setOrder('name','asc');
+		$item->setOrder('display_sequence','desc');
+		//$item->setOrder('name','asc');
 
 		$layout_template = $this->options['layout'];
 		
