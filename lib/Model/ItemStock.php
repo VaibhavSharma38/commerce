a29da671391d334393855584abfe01218c7a2004
vaibhavsharma38 <vaibhavsharma38@gmail.com>
2017-09-29 15:19:32 +0530

    changes made

--- lib/Model/ItemStock.php
+++ lib/Model/ItemStock.php
@@ -46,6 +46,25 @@ class Model_ItemStock extends \xepan\base\Model_Table{
 	function importStock($data){
 		// deleting old values
 		$this->add('xepan\commerce\Model_ItemStock')->deleteAll();
+		
+		// deleting item size custom field 
+		$item_m = $this->add('xepan\commerce\Model_Item');
+
+		foreach ($item_m as $item) {
+			$asso = $this->add('xepan\commerce\Model_Item_CustomField_Association')
+						 ->addCondition('item_id',$item->id);
+
+			$asso->addCondition('CustomFieldType','CustomField');
+			$asso->addCondition('name','Size');
+			$asso->tryLoadAny();
+
+			$model_cf_value = $this->add('xepan\commerce\Model_Item_CustomField_Value');
+			$model_cf_value->addCondition('customfield_association_id',$asso->id);
+			
+			foreach ($model_cf_value as $cf) {
+				$cf->delete();
+			}
+		}
 
 		// multi record loop
 		foreach ($data as $key => $record) {
--- page/test1.php
+++ page/test1.php
@@ -7,22 +7,6 @@ Class page_test1 extends \xepan\base\Page{
 	function init(){
 		parent::init();		
 		
-		$item_m = $this->add('xepan\commerce\Model_Item');
-
-		foreach ($item_m as $item) {
-			$asso = $this->add('xepan\commerce\Model_Item_CustomField_Association')
-						 ->addCondition('item_id',$item->id);
-
-			$asso->addCondition('CustomFieldType','CustomField');
-			$asso->addCondition('name','Size');
-			$asso->tryLoadAny();
-
-			$model_cf_value = $this->add('xepan\commerce\Model_Item_CustomField_Value');
-			$model_cf_value->addCondition('customfield_association_id',$asso->id);
-			
-			foreach ($model_cf_value as $cf) {
-				$cf->delete();
-			}
-		}
+		
 	}
 }
